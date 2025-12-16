96e145d9bcf20251ff1a2b688e1e1ccbffe35024
M-OssaOliveira <molekamari@gmail.com>
2025-12-16 11:56:38 -0300

    first commit uml-sb-jpa aulas curso uml

---
+++ src/main/java/com/uml/springbootjpa/uml_springboot_jpa/UmlSpringbootJpaApplication.java
@@ -0,0 +1,13 @@
+package com.uml.springbootjpa.uml_springboot_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmlSpringbootJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmlSpringbootJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uml/springbootjpa/uml_springboot_jpa/UmlSpringbootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uml.springbootjpa.uml_springboot_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmlSpringbootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
